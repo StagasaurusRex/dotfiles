7e20dc3c421ec4ebb3ed956718b2414f1d8dc166
Staggs <staggs@bifid.local>
2017-11-28 20:49:55 -0600

    :sparkles: Add password handling to bootstrap.sh

--- home/.bootstrap/.bootstrap.sh
+++ home/.bootstrap/.bootstrap.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+osascript -e 'tell application "System Preferences" to quit'
+
+sudo -v
+
+while true; do sudo -n true; sleep 60; \
+    kill -0 "$$" || exit; done 2>/dev/null &
+
 # Software Update
 
 echo ""
